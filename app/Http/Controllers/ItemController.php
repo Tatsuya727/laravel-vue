13318036035c95d35c5d494c1deb1e8d77fadb27
tatsuya <babatsuya@icloud.com>
2023-06-09 17:17:59 +0900

    商品編集ページ

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -60,7 +60,9 @@ class ItemController extends Controller
      */
     public function edit(Item $item)
     {
-        //
+        return Inertia::render("Items/Edit", [
+            "item" => $item
+        ]);
     }
 
     /**
@@ -68,7 +70,7 @@ class ItemController extends Controller
      */
     public function update(UpdateItemRequest $request, Item $item)
     {
-        //
+        
     }
 
     /**

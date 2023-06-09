40f4349fc442e11ebd83f745c0372e37f9e34bfc
tatsuya <babatsuya@icloud.com>
2023-06-09 13:53:15 +0900

    items index

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ItemSeeder extends Seeder
 {
@@ -12,6 +13,22 @@ class ItemSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table("items")->insert([
+            [
+                "name" => "カット",
+                "memo" => "カットのメモ",
+                "price" => 6000,
+            ],
+            [
+                "name" => "カラー",
+                "memo" => "カラーのメモ",
+                "price" => 8000,
+            ],
+            [
+                "name" => "パーマ",
+                "memo" => "パーマのメモ",
+                "price" => 13000,
+            ]
+        ]);
     }
 }

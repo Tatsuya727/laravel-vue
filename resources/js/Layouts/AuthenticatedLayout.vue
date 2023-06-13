ddbf9b5c0e506b4b57c1006a8664369a9c88284a
tatsuya <babatsuya@icloud.com>
2023-06-13 14:50:02 +0900

    購入履歴のナビゲーション

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -29,6 +29,7 @@ const showingNavigationDropdown = ref(false);
                             <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')"> Dashboard </NavLink>
                                 <NavLink :href="route('purchases.create')" :active="route().current('purchases.create')"> 購入画面 </NavLink>
+                                <NavLink :href="route('purchases.index')" :active="route().current('purchases.index')"> 購入履歴 </NavLink>
                                 <NavLink :href="route('items.index')" :active="route().current('items.index')"> 商品管理 </NavLink>
                                 <NavLink :href="route('customers.index')" :active="route().current('customers.index')"> 顧客管理 </NavLink>
                             </div>
@@ -93,6 +94,7 @@ const showingNavigationDropdown = ref(false);
                     <div class="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')"> Dashboard </ResponsiveNavLink>
                         <ResponsiveNavLink :href="route('purchases.create')" :active="route().current('purchases.create')"> 購入画面 </ResponsiveNavLink>
+                        <ResponsiveNavLink :href="route('purchases.index')" :active="route().current('purchases.index')"> 購入履歴 </ResponsiveNavLink>
                         <ResponsiveNavLink :href="route('items.index')" :active="route().current('items.index')"> 商品管理 </ResponsiveNavLink>
                         <ResponsiveNavLink :href="route('customers.index')" :active="route().current('customers.index')"> 顧客管理 </ResponsiveNavLink>
                     </div>
